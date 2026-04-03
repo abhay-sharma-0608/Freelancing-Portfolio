32104c20b2a2eaa901e8086e676e5ae34ad86136
Abhay Sharma <abhay030104@gmail.com>
2026-04-03 16:54:37 +0530

    Clean up Navbar.js by removing commented code
    Removed commented-out code for resume link in Navbar component.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,15 +6,12 @@ import avatar from "../Assets/avatar.svg";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
-// import { ImBlog } from "react-icons/im";
 import {
   AiFillStar,
   AiOutlineHome,
   AiOutlineFundProjectionScreen,
   AiOutlineUser,
 } from "react-icons/ai";
-
-import { CgFileDocument } from "react-icons/cg";
 
 function NavBar() {
   const [expand, updateExpanded] = useState(false);
@@ -81,16 +78,6 @@ function NavBar() {
                 Projects
               </Nav.Link>
             </Nav.Item>
-
-            // <Nav.Item>
-            //   <Nav.Link
-            //     as={Link}
-            //     to="/resume"
-            //     onClick={() => updateExpanded(false)}
-            //   >
-            //     <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-            //   </Nav.Link>
-            // </Nav.Item>
 
             <Nav.Item className="fork-btn">
               <Button
